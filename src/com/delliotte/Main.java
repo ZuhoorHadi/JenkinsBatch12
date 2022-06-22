eae25beb565a6d4431183a8392bea1c0772971b6
Zuhoor Hadi <zuhoorhadi@gmail.com>
2022-06-21 21:40:32 -0400

    First Commit

---
+++ src/com/delliotte/Main.java
@@ -0,0 +1,11 @@
+package com.delliotte;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for (int i=0; i<5; i++){
+        System.out.println("hello! I am SDET learning Jenkins");
+        
+    }
+    }
+}
